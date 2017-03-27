e8881077241d3c16f4ad2de032a7b76eb6ea0c2b
Marcello Romani <marcello.romani@magentys.io>
2017-03-27 23:54:39 +0100

    Revert "Add fix in b52641d for issue #9"
    This reverts commit c34bc14b155b51ff8542b68629a588323011fae5.

--- SimpleTimer/SimpleTimer.cpp
+++ SimpleTimer/SimpleTimer.cpp
@@ -145,7 +145,7 @@ int SimpleTimer::setupTimer(unsigned long d, void* f, void* p, boolean h, unsign
         return -1;
     }
 
-    if (f == NULL || n == 0) {
+    if (f == NULL) {
         return -1;
     }
 
